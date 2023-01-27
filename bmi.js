e53defec97779992d5f515f3a232bb6efe1a3ce9
nicroco <nicola.pandolfi15@gmail.com>
2023-01-27 12:56:14 +0100

    updated

--- bmi.js
+++ bmi.js
@@ -4,13 +4,11 @@ function calculateBMI() {
   var height = document.getElementById("height").value;
   var result = weight / (height / 100 * height / 100);
   var status;
-  var kgNeeded = 0;
 
   if (result < 18.5) {
     status = "Underweight";
   } else if (result >= 18.5 && result < 25) {
     status = "Normal weight";
-    kgNeeded = (25 * (height / 100 * height / 100)) - weight;
   } else if (result >= 25 && result < 30) {
     status = "Overweight";
   } else {
@@ -21,11 +19,6 @@ function calculateBMI() {
   document.getElementById("status").innerHTML = status;
   document.getElementById("result-label").style.visibility = "visible";
   document.getElementById("status-label").style.visibility = "visible";
-
-  if (status === "Normal weight" || status === "Underweight") {
-    document.getElementById("kg-needed-label").innerHTML = "You're " + kgNeeded.toFixed(2) + " kg from being overweight";
-    document.getElementById("kg-needed-label").style.visibility = "visible";
-  } else {
-    document.getElementById("kg-needed-label").style.visibility = "hidden";
-  }
+  document.getElementById("result").style.visibility = "visible";
+  document.getElementById("status").style.visibility = "visible";
 }
